6808ee9b62350898df2507dd50761f922dde1925
Rahmandi Fitra Madenda <99945506+randyisalive@users.noreply.github.com>
2024-12-17 16:25:51 +0700

    Update page.jsx

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -7,16 +7,9 @@ const LoginPage = () => {
   return (
     <div className="flex h-screen">
       <div
-        className="w-3/6 login-background text-white text-3xl p-3 hidden md:block"
+        className="w-3/6 login-background text-white text-3xl hidden md:block"
         style={{ backgroundColor: "#1E56A0" }}
-      >
-        <Image
-          src="https://indomonangjadi.com/wp-content/uploads/2023/11/Logo-Default.png"
-          alt="Description of the image"
-          width={300}
-          height={200}
-        />
-      </div>
+      ></div>
       <div className="w-full p-3 justify-center items-center flex  md:w-3/6">
         <FormControl title="Secure Client Login" className={`w-96 shadow-md`} />
       </div>
