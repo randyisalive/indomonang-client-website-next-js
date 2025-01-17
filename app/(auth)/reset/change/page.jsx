2309cce60b09ee017c1f2d94be47083154773273
Rahmandi Fitra Madenda <99945506+randyisalive@users.noreply.github.com>
2025-01-17 13:55:24 +0700

    Update page.jsx

--- app/(auth)/reset/change/page.jsx
+++ app/(auth)/reset/change/page.jsx
@@ -1,11 +1,11 @@
 import Navbar from "@/app/components/Navbar";
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 import FormChangePassword from "./components/FormChangePassword";
 
 const ResetChildPage = () => {
   return (
-    <div>
+    <Suspense fallback="Loading...">
       <div className="bg-mainBlue flex lg:justify-center p-5 h-screen">
         <div className="flex flex-col p-5    lg:w-2/6  border bg-white rounded-md">
           <span className="text-4xl mt-3 font-bold w-full text-center">
@@ -25,7 +25,7 @@ const ResetChildPage = () => {
           </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
