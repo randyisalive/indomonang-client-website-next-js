f6b845e2419ae99b80382884e7e71bb8aa28f7cd
Rahmandi Fitra Madenda <99945506+randyisalive@users.noreply.github.com>
2025-02-04 12:07:08 +0700

    delete highest transaction

--- app/components/ui/tableComponent/Invoice (payment history)/PaymentHistorySection.jsx
+++ app/components/ui/tableComponent/Invoice (payment history)/PaymentHistorySection.jsx
@@ -41,7 +41,7 @@ const PaymentHistorySection = ({ data = [] }) => {
     refund_total / rejected_filtered.length
   ).toFixed(0);
   return (
-    <div className="py-3 px-5 lg:px-0 gap-5 lg:gap-0  w-full flex flex-col lg:flex-row text-sm">
+    <div className="py-3 px-5 lg:px-0 gap-5 lg:gap-0  w-full flex flex-col lg:flex-row text-sm justify-between">
       <div className="lg:w-1/3 lg:justify-start flex  justify-center">
         <div className="flex flex-col gap-2 items-center lg:items-start">
           <span className=" underline text-xs lg:text-base">
@@ -63,23 +63,7 @@ const PaymentHistorySection = ({ data = [] }) => {
           </div>
         </div>
       </div>
-      <div className="lg:w-1/3 justify-center items-center  flex">
-        <div className="flex flex-col gap-2 items-center ">
-          <span className="underline text-xs lg:text-base">
-            Highest Transaction
-          </span>
-          <NumberFlow
-            value={result_max}
-            prefix="Rp. "
-            suffix=" ,-"
-            className="lg:text-3xl text-xl font-bold"
-          />
-          <div className="flex flex-col text-xs lg:text-base">
-            <span>Min. Transaction</span>
-            <span>Rp. {result_min.toLocaleString("id-ID")} ,-</span>
-          </div>
-        </div>
-      </div>
+
       <div className="lg:w-1/3   flex lg:justify-end justify-center ">
         <div className="flex flex-col items-center lg:items-start gap-2">
           <span className="underline text-xs lg:text-base">Refunds </span>
